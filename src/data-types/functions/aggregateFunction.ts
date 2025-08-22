f0e3802fd1799fbb0e17907a8ae2fa65df02f479
Mauritz Dressler <mauritz@prodport.com>
2025-08-22 11:51:55 +0000

    📈 aggregateFunction | 🆕 helper type to get intellisense information for schema

--- src/data-types/functions/aggregateFunction.ts
+++ src/data-types/functions/aggregateFunction.ts
@@ -1,13 +1,6 @@
 import { type AnyDataType, type DataType, DataTypes } from "../../_mod.ts";
 import { literal, stringifyDataType } from "../../stringify-data-type.ts";
 
-/** @see https://clickhouse.com/docs/sql-reference/data-types/aggregatefunction */
-export type CHAggregateFunction<ArgumentTypes extends AnyDataType[]> = DataType<
-  DataTypes.AggregateFunction,
-  unknown,
-  ArgumentTypes
->;
-
 export type FunctionTypes =
   | "count"
   | "uniq"
@@ -21,17 +14,51 @@ export type FunctionTypes =
   | "argMin"
   | "argMax";
 
+/** Helper type to make TypeScript show some helpful type descriptor of {@link CHAggregateFunction} */
+type AggregateFunction<Type extends FunctionTypes, Arguments> = {
+  functionType: Type;
+  argumentTypes: Arguments;
+};
+
+/** Helper type to convert an aggregate function to the {@link AggregateFunction} TypeScript helper type */
+type AggregateFunctionToTs<
+  Type extends FunctionTypes,
+  ArgumentTypes extends AnyDataType[]
+> = AggregateFunction<
+  Type,
+  ArgumentTypes[number] extends DataType<
+    DataTypes,
+    infer R,
+    AnyDataType[] | undefined
+  >
+    ? R
+    : unknown
+>;
+
+/** @see https://clickhouse.com/docs/sql-reference/data-types/aggregatefunction */
+export type CHAggregateFunction<
+  Type extends FunctionTypes,
+  ArgumentTypes extends AnyDataType[]
+> = DataType<
+  DataTypes.AggregateFunction,
+  AggregateFunctionToTs<Type, ArgumentTypes>,
+  ArgumentTypes
+>;
+
 /**
  * Creates a new `AggregateFunction(function_name, types_of_arguments...)` ClickHouse value
  *
  * @see https://clickhouse.com/docs/sql-reference/data-types/aggregatefunction
  */
-export const aggregateFunction = <ArgumentTypes extends AnyDataType[]>(c: {
-  funcType: FunctionTypes;
+export const aggregateFunction = <
+  Type extends FunctionTypes,
+  ArgumentTypes extends AnyDataType[]
+>(c: {
+  funcType: Type;
   argTypes?: ArgumentTypes;
   description?: string;
   default?: unknown;
-}): CHAggregateFunction<ArgumentTypes> => {
+}): CHAggregateFunction<Type, ArgumentTypes> => {
   const args = [literal(c.funcType)];
   if (c.argTypes && c.argTypes.length > 0) {
     args.push(...c.argTypes.map((t) => literal(stringifyDataType(t))));
@@ -39,7 +66,10 @@ export const aggregateFunction = <ArgumentTypes extends AnyDataType[]>(c: {
 
   return {
     type: DataTypes.AggregateFunction,
-    typeScriptType: undefined as unknown,
+    typeScriptType: undefined as unknown as AggregateFunctionToTs<
+      Type,
+      ArgumentTypes
+    >,
     innerTypes: c.argTypes,
     arguments: args,
     description: c.description,
